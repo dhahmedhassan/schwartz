cc7451ad09db599c4e955f8477a329f87069bb23
dhahmedhassan <dhahmedhassan@protonmail.com>
2019-03-18 03:00:27 +0200

    add questions views in QuestionsController

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,7 +14,7 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        //
+        return view('questions.index');
     }
 
     /**
@@ -24,7 +24,7 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        return view('questions.create');
     }
 
     /**
