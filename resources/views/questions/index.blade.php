a09aa8a7eb5e6982cb432839294471d2a6469615
dhahmedhassan <dhahmedhassan@protonmail.com>
2019-03-18 05:43:30 +0200

    show questions in questions.index view

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -2,4 +2,27 @@
 
 @section('content')
     <h1>Questions Index</h1>
+
+    <form action="/questionnaires/create" method="get">
+        {{ csrf_field() }}
+        <div class="card">
+            @foreach ($questions as $question)
+                <div class="form-check form-check-inline">
+                    <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" name="id[{{ $question->id }}]" value="{{ $question->id }}" id="{{ $question->id }}">
+                            <strong>
+                                {{ $question->rog_num }} &nbsp;)&nbsp;
+                                @if ($question->subpart !== '')
+                                    &nbsp;({{ $question->subpart }})&nbsp;
+                                @endif 
+                            </strong> 
+                            {{ $question->body }}
+                    </label>
+                </div>
+
+                @endforeach
+        </div>
+   
+        <button type="submit" id="submit" class="btn btn-primary ml-auto">submit</button>
+    </form>
 @endsection
