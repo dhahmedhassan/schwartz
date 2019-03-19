53842964f4b9dc5869674845ea795042cea1ce94
dhahmedhassan <dhahmedhassan@protonmail.com>
2019-03-19 06:28:40 +0200

    show Questionnaires in questionnaires.index view

--- resources/views/questionnaires/index.blade.php
+++ resources/views/questionnaires/index.blade.php
@@ -3,4 +3,40 @@
 @section('content')
     <h1>Questionnaire Index</h1>
     
+    @foreach ($questionnaires as $questionnaire)
+        <center>
+            <h3>
+                Questionnaire Id: {{ $questionnaire->id }}
+             </h3>
+        </center>
+       
+       @foreach ($questionnaire->questionnaire_details as $details)
+       
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">
+                        Question &nbsp;  
+                        @foreach ($questions as $question)
+                            @if ($details->question_id == $question->id )
+                            <b>{{ $question->rog_num }} &nbsp;</b>
+                            @if (!$question->subpart == '')
+                                <b>{{ $question->subpart }}) &nbsp;</b>
+                            @endif
+                                {{ $question->body }}
+                            @endif    
+                            
+                        @endforeach
+                    </h4>
+                    <p class="card-text">
+                        Answer Body &nbsp; : {{ $details->body }}
+                    </p>
+                    <p class="card-text">
+                        Answer Created at &nbsp; : {{ $details->created_at }}
+                    </p>
+                </div>
+            </div>
+
+       @endforeach
+       <hr>
+    @endforeach
 @endsection
