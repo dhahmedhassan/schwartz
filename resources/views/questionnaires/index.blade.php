cd8e71d8e105cc71005b582c4903551bc1e6285e
dhahmedhassan <dhahmedhassan@protonmail.com>
2019-03-19 08:37:45 +0200

    leave only the index of the each questionnaire

--- resources/views/questionnaires/index.blade.php
+++ resources/views/questionnaires/index.blade.php
@@ -6,37 +6,11 @@
     @foreach ($questionnaires as $questionnaire)
         <center>
             <h3>
-                Questionnaire Id: {{ $questionnaire->id }}
+                <a href="/questionnaires/{{ $questionnaire->id }}">
+                    Questionnaire Id: {{ $questionnaire->id }}
+                </a>
              </h3>
         </center>
        
-       @foreach ($questionnaire->questionnaire_details as $details)
-       
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title">
-                        Question &nbsp;  
-                        @foreach ($questions as $question)
-                            @if ($details->question_id == $question->id )
-                            <b>{{ $question->rog_num }} &nbsp;</b>
-                            @if (!$question->subpart == '')
-                                <b>{{ $question->subpart }}) &nbsp;</b>
-                            @endif
-                                {{ $question->body }}
-                            @endif    
-                            
-                        @endforeach
-                    </h4>
-                    <p class="card-text">
-                        Answer Body &nbsp; : {{ $details->body }}
-                    </p>
-                    <p class="card-text">
-                        Answer Created at &nbsp; : {{ $details->created_at }}
-                    </p>
-                </div>
-            </div>
-
-       @endforeach
-       <hr>
     @endforeach
 @endsection
