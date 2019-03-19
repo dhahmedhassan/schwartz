3ce0fc63f979826a70b7c75ebdc7d8a853b0a31a
dhahmedhassan <dhahmedhassan@protonmail.com>
2019-03-19 18:58:12 +0200

    show form questions data in forms.show

--- resources/views/forms/show.blade.php
+++ resources/views/forms/show.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>A Place to show single Single</h1>
+    <h1>Form {{ $form[0]->id }}</h1>
+    @if ($form)
+        @foreach ($form[0]->questions as $item)
+
+                <div class="card">
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label" id="{{ $item->id }}">
+                            <input class="form-check-input" type="checkbox" name="id[{{ $item->id }}]" id="{{ $item->id }}" value="{{ $item->id }}"> 
+                            <b>
+                                    {{ $item->rog_num }} &nbsp;
+                                    @if ($item->subpart !== '')
+                                        ({{ $item->subpart }}) &nbsp;
+                                    @endif
+                                </b>
+                            {{ $item->body }}
+                        </label>
+                    </div>
+
+                </div>
+
+        @endforeach
+    @endif
 @endsection
